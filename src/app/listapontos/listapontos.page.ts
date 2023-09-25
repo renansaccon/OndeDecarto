5f2afd1e4b5a4ecde5f68e25b6442a93a62aca77
renansaccon <sacconrenan@gmail.com>
2023-09-25 00:38:07 -0300

    Adição de novas Categorias

--- src/app/listapontos/listapontos.page.ts
+++ src/app/listapontos/listapontos.page.ts
@@ -54,6 +54,159 @@ export class ListapontosPage implements OnInit {
       "telefone": "(43) 2103-9950",
       "cidade": "Londrina"
     },
+    {
+      "id": "5",
+      "categoria": "Pilhas",
+      "nome": "Hayamax Distribuidora de Produtos Eletrônicos",
+      "endereco": "Rua Senador Souza Naves",
+      "numero": "9",
+      "telefone": "(43) 3377-6800",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "6",
+      "categoria": "Pilhas",
+      "nome": "Leroy Merlin",
+      "endereco": "Avenida Theodoro Victorelli",
+      "numero": "60",
+      "telefone": "0800 020 5376",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "7",
+      "categoria": "Pilhas",
+      "nome": "Kalunga",
+      "endereco": "Avenida Theodoro Victorelli",
+      "numero": "150",
+      "telefone": "(11) 3346-9966",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "8",
+      "categoria": "Pilhas",
+      "nome": "E-letro",
+      "endereco": "Rua Ermelindo Leão",
+      "numero": "385",
+      "telefone": "(43) 3339-0475",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "9",
+      "categoria": "Pilhas",
+      "nome": "Eletrônica Valtech Ltda",
+      "endereco": "Rua Belo Horizonte",
+      "numero": "117",
+      "telefone": "(43) 3321-0077",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "10",
+      "categoria": "Madeira",
+      "nome": "Ponto de Entrega Voluntária",
+      "endereco": "Rua Capitão João Busse",
+      "numero": "1274-1466",
+      "telefone": "(43) 3379-7900",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "11",
+      "categoria": "Madeira",
+      "nome": "Ponto de Entrega Voluntária",
+      "endereco": "Rua Helmuth Fischer",
+      "numero": " S/N",
+      "telefone": "--",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "12",
+      "categoria": "Pneu",
+      "nome": "Adriano de Almeida Pneus Ltda",
+      "endereco": "Rua Amélia Riskallah Abib Tauil",
+      "numero": "1337",
+      "telefone": "(43) 3367-5071",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "13",
+      "categoria": "Pneu",
+      "nome": "Engesol Engenharia e Soluções Ltda",
+      "endereco": "Rua Grafita",
+      "numero": "563",
+      "telefone": "(43) 99622-4853 / (43) 9829-4889",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "14",
+      "categoria": "Entulho",
+      "nome": "Ponto de Entrega Voluntária",
+      "endereco": "Rua Capitão João Busse",
+      "numero": "1274-1466",
+      "telefone": "(43) 3379-7900",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "15",
+      "categoria": "Entulho",
+      "nome": "Ponto de Entrega Voluntária",
+      "endereco": "Rua Helmuth Fischer",
+      "numero": " S/N",
+      "telefone": "--",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "16",
+      "categoria": "Medicamentos",
+      "nome": "Farmácia Nissei",
+      "endereco": "Av. Tiradentes",
+      "numero": "220",
+      "telefone": "4004-4041",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "17",
+      "categoria": "Medicamentos",
+      "nome": "Farmácia Nissei",
+      "endereco": "Av. Higienópolis",
+      "numero": "1198",
+      "telefone": "4004-4041",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "17",
+      "categoria": "Medicamentos",
+      "nome": "Farmácia Droga Raia",
+      "endereco": "Rua São Pedro",
+      "numero": "380",
+      "telefone": "(43) 99176-4018",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "18",
+      "categoria": "Medicamentos",
+      "nome": "Farmácia Vale Verde",
+      "endereco": "Av. Arthur Thomas",
+      "numero": "1100",
+      "telefone": "(43) 3379-0001",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "18",
+      "categoria": "Medicamentos",
+      "nome": "Farmácia Phloraceae",
+      "endereco": "Av. Senador Souza Naves",
+      "numero": "929",
+      "telefone": "(43) 3379-0001",
+      "cidade": "Londrina"
+    },
+    {
+      "id": "19",
+      "categoria": "Óleo Doméstico",
+      "nome": "Cáritas Arquidiocesana de Londrina",
+      "endereco": "Rua Dom Bosco",
+      "numero": "145",
+      "telefone": "(43) 3338-7252",
+      "cidade": "Londrina"
+    },
   ];
 
   constructor (
